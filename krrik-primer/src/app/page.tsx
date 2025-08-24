66481b4a48d5add0c4a719b49b2fb6495cf4fd4a
edg <evanm.deguzman@gmail.com>
2025-08-24 19:45:26 +0800

    feat: completed gameplan section

--- krrik-primer/src/app/page.tsx
+++ krrik-primer/src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
           </div>
         </section>
         <section id="WhyK'rrik" className="py-16 md:py-24 lg:py-32 px-5 flex flex-col md:flex-row items-start mt-80 gap-8">
-          <div className="flex-1">
+          <div className="flex-1 pl-5">
             <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8">WHY K&apos;RRIK?</h2>
             <p className="text-lg md:text-xl text-gray-300 text-justify">
               I have many reasons as to why I had wanted and love to play K&apos;rrik. Firstly, its that core mechanic of Phyrexian mana which is <span className="font-bold">paying life</span> instead.
@@ -60,6 +60,31 @@ export default function Home() {
               className="object-cover rounded-2xl"
             />
           </div>
+        </section>
+        <section id="gameplan" className="py-16 md:py-24 lg:py-32 px-5 flex flex-col md:flex-row items-start mt-40 gap-8">
+          <div className="flex-1 flex flex-col items-center">
+            <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8 text-center">GOAL OF THE DECK</h2>
+            <p className="text-lg md:text-xl text-gray-300 pr-30 pl-30 text-justify">
+              The deck has a simple goal: <span className="font-bold">PAY LIFE</span>. To be more specific, you will damage yourself to gain advantages over opponents. That self-damage may lead to more spells being brought out or punishing your opponents for allowing you to go so low ( <span className="italic">ahem, Repay in Kind </span>).
+              <br></br><br></br>
+              Not only that but this deck allow you to gain life and then some. Many combo lines allow you to gain life, and if that is not possible, well then many creatures and singular instants and sorceries allow you to either gain life or punish others for not allowing to do so ( <span className="italic"> Repay in Kind, again </span>).
+              <br></br><br></br>
+              Essentially, you will play as quite literally the most mono-black way. You will sacrifice creatures and even yourself to come out with a win. It is simplistic perfection: pay life, gain life, kill all that stands in your way.
+            </p>
+          </div>
+          <div className="flex-1 flex flex-col items-center">
+            <h2 className="text-4xl md:text-6xl lg:text-7xl font-bold mb-8 text-center">GOAL IN PODS</h2>
+            <p className="text-lg md:text-xl text-gray-300 pr-30 pl-30 text-justify">
+              There is something that you must first tell yourself should you ever want to play a deck like this, or any K&apos;rrik deck for that matter, and it is that <span className="font-bold">you are the villain.</span> Whether you like it or not, the moment you sit down for a game of commander, you are the threat.
+              <br></br><br></br>
+              With that being said, your primary goal early game is to make sure you are healthy. They will attack you first and make sure you are limited in the amount of life you can pay. After that, once they feel like they have curbed you enough, they will likely not target you anymore.
+              As much as it sucks though, you have to make sure you are healthy enough to be able to do combo lines to gain life. Stay in the game by utlizing K&apos;rrik&apos;s lifelink with his counters as well as other&apos;s lifelinks.
+              <br></br><br></br>
+              Once you have more than enough life, you can now enact your combos. Find your cards, draw or tutor them out, then do your combos. You will have your time, you just simply have to buy enough of it for yourself and they will rue the day they even let you live.
+            </p>
+          </div>
+        </section>
+        <section id="musthaves">
         </section>
       </main>
     </div>
